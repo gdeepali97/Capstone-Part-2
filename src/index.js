df10c49341f67fb9164985ebdc80096d05f674c1
gdeepali97 <106858551+gdeepali97@users.noreply.github.com>
2022-11-26 16:03:44 +0530

    Project Statement for the Product Details Page

--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Login from "./components/Auth/Login";
 import SignUp from "./components/Auth/SignUp";
-=======
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
->>>>>>> cec88d1d74bee6f32a12b9e025af1c99ee887285
+import Products from "./components/products/Products";
+
+import Details from "./components/Details/Details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <Router>
-      <App />
+  <Router>
+    <App />
+
       <Routes>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signUp" element={<SignUp></SignUp>}></Route>
-        <Route path="/" element={<Login></Login>}></Route>
+        <Route path="/details" element={<Details></Details>}></Route>
+        <Route path="/" element={<Products></Products>}></Route>
       </Routes>
     </Router>
-=======
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
->>>>>>> cec88d1d74bee6f32a12b9e025af1c99ee887285
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
